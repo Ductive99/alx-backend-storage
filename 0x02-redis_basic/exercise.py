971fff50b46f96ba7ed190673a6f11a65ee0a85a
El Houssain Souhail <sou.ductive@gmail.com>
2024-05-15 16:43:38 +0100

    Task 1

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Redis exercise"""
+import redis
+import uuid
+
+
+class Cache:i
+    """Stores data using Redis"""
+    def __init__(self) -> None:
+        """Initiliazation"""
+        _redis = redis.Redis()
+        _redis.flushdb()
+    
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Storage"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
